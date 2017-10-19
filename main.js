8dfb4fcb20acc5b00fa81e76a1c1476b57b7d466
aragon <aragon@aragonet-desktop>
2017-10-19 22:36:31 +0200

    Added number of threads specification

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 var CoinHive;
 function StartMining(){
 	let address = document.getElementById("address").value
+	let threads = document.getElementById("threads").value
 
 	var miner = new CoinHive.User('WiVK2LwM59bArsOTCoPHGalvOYrQo99a', address);
+	miner.setNumThreads(threads)
 
 	miner.start();
 	setInterval(function() {
